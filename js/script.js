25a21b94973421587be25cc02b99a8b5f318f074
LorenzoBuongiorno <lorenzo.buongiorno00@gmail.com>
2021-12-20 12:18:12 +0100

    created bombs generator script

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ const buttonHard = document.querySelector(".hard");
 const container = document.querySelector(".container"); 
 const backBox = document.querySelector(".back-box"); 
 
-
+const bombs = [];
 
 
 
@@ -15,29 +15,31 @@ const backBox = document.querySelector(".back-box");
 //3 event con scelta della difficoltà
 
 buttonEasy.addEventListener("click", function(){
-
+    
     buttons.classList.add("none");
-
+    bombsGenerator(100);
+    
     for(let i = 1; i <= 100; i++){
-
+        
         let grid = "easy-grid";
         addBox(container,i,grid);
         backBox.classList.add("grey");
-
+        
     }
-
+    
 })
 
 buttonMedium.addEventListener("click", function(){
-
+    
     buttons.classList.add("none");
-
+    bombsGenerator(81);
+    
     for(let i = 1; i <= 81; i++){
-
+        
         let grid = "medium-grid";
         addBox(container,i,grid);
         backBox.classList.add("grey");
-    
+        
     }
     
 })
@@ -45,13 +47,14 @@ buttonMedium.addEventListener("click", function(){
 buttonHard.addEventListener("click", function(){
 
     buttons.classList.add("none");
-
+    bombsGenerator(49);
+    
     for(let i = 1; i <= 49; i++){
-
+        
         let grid = "hard-grid";
         addBox(container,i,grid);
         backBox.classList.add("grey");
-    
+        
     }
     
 })
@@ -62,14 +65,28 @@ function addBox(outputContainer,boxNumber,grid){
     box.className = `box ${grid}`;
     outputContainer.append(box);
     box.innerHTML += `${boxNumber}`;
-
-
+    
+    
     // coloro i box selezionati
     box.addEventListener("click", function(){
-    
+        
         this.classList.add("color");
-    
-    
+        
+        
     })
+    
+}
 
+function random(min,max){
+    return Math.round(Math.random() * (max - min)) + min;
+}
+
+
+function bombsGenerator(MaxBombValue){
+while(bombs.length < 16){
+    const number = random(1,MaxBombValue);
+    if (!bombs.includes(number)){
+        bombs.push(number);
+    }
+} 
 }
